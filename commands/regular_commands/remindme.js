7cc3f11bd6615352af1d463d64eef825b3de7f7e
Cyryll Galon <galoncyryll@gmail.com>
2020-02-05 13:38:43 -0800

    update remind me feature

--- commands/regular_commands/remindme.js
+++ commands/regular_commands/remindme.js
@@ -1,7 +1,23 @@
-const scheduler = require('node-schedule');
+const ms = require('ms');
 
-module.exports.run = (bot, message, args, NaM, OMGScoots) => {
-  console.log(args);
+module.exports.run = (bot, message, args, NaM) => {
+  if (args[0] === 'help') {
+    return message.channel.send('```Usage: $remindme <time> <message>```');
+  }
+
+  let time = args[0];
+  let rmdMessage = args.slice(1).join('');
+
+  message.reply(`I will send you a message in ${time} ${NaM}`);
+  setTimeout(() => {
+    try {
+      message
+        .author
+        .send(rmdMessage);
+    } catch (e) {
+      return message.reply('Your DMs are locked. I can\'t send mod commands');
+    }
+  }, ms(time));
 };
 
 module.exports.help = {
