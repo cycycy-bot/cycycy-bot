3933a8027a53bd5a84e28c55f7c1477c125db26a
Cyryll Galon <galoncyryll@gmail.com>
2020-03-26 14:18:57 -0700

    revert changes

--- base/Twitch.js
+++ base/Twitch.js
@@ -2,7 +2,6 @@ const tmi = require('tmi.js');
 require('dotenv').config();
 
 module.exports = (bot) => {
-  const twitchChannel = bot.channels.get('692652832481869884');
   const client = new tmi.Client({
     connection: {
       reconnect: true,
@@ -16,8 +15,9 @@ module.exports = (bot) => {
   });
 
   client.on('message', (channel, tags, message, self) => {
+    const twitchDiscordChannel = bot.channels.get('692652832481869884');
     if (self) return;
-    twitchChannel.send(`\`${tags.username}\`: ${message}`);
+    twitchDiscordChannel.send(`\`${tags.username}\`: ${message}`);
   });
 
   client.connect();
