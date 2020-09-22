2dd5b7db2c7b43eb6477f51d47d3ad0f3c451957
cyryll <galoncyryll@gmail.com>
2020-09-22 15:41:05 -0700

    add fact command

---
+++ commands/twitch_commands/fact.js
@@ -0,0 +1,24 @@
+const Command = require('../../base/Command');
+
+class Advice extends Command {
+  constructor(bot) {
+    super(bot, {
+      name: 'fact',
+      description: 'Gives random fact',
+      usage: '$fact',
+      cooldown: 10000,
+    });
+  }
+
+  async run(message, args) {
+    const url = 'https://uselessfacts.jsph.pl/random.json?language=en';
+    this.bot.fetch(url)
+      .then(res => res.json())
+      .then((res) => {
+        this.bot.say(message.channelName, `Random Useless Fact 4Head : ${res.text}`);
+      })
+      .catch(err => console.error(err));
+  }
+}
+
+module.exports = Advice;
