f2b8f8a440fb33abfb3f1577d7db994d8eef0689
Cyryll Galon <cgalon@healthcarefinancedirect.com>
2021-02-27 14:35:37 -0800

    remove thesaurus command and fix emote

--- handlers/typingStart.js
+++ handlers/typingStart.js
@@ -2,6 +2,6 @@ module.exports = (bot, channel, user) => {
   if (channel.name === 'general' && user.id === '216752284791603202') {
     bot.channels.cache
       .get(channel.id)
-      .send('ambulung is typing in general <a:docLeave:776882312830124082>');
+      .send('ambulung is typing in general <a:docLeave:815351108826038282>');
   }
 };
