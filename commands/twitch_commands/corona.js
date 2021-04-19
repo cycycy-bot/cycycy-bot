44b8a1dbe1356cb6c9a221e9989a9a70e73546b8
cybothelper <cybothelper@yandex.com>
2021-04-19 17:26:59 +0000

    Fix missing formatting on covid stats

--- commands/twitch_commands/corona.js
+++ commands/twitch_commands/corona.js
@@ -22,10 +22,14 @@ class Corona extends Command {
       const stateArg = args.slice(1).join(' ');
       return track.states().then((stateArray) => {
         stateArray.forEach((stateData) => {
-          if (stateData.state.toLowerCase() === stateArg.toLowerCase()) {
-            const {
-              cases, deaths, todayCases, todayDeaths, state,
-            } = stateData;
+            if (stateData.state.toLowerCase() === stateArg.toLowerCase()) {
+		let {
+		    cases, deaths, todayCases, todayDeaths, state,
+		} = stateData;
+		cases = Number(cases).toLocaleString();
+		deaths = Number(deaths).toLocaleString();
+		recovered = Number(recovered).toLocaleString();
+		todayCases = Number(todayCases).toLocaleString();g
             const coronaStringResult = `${state}'s Corona Virus Information: ${cases} Confirmed Cases, ${deaths} Deaths, ${todayCases} Cases Today, ${todayDeaths} Deaths Today`;
             return this.bot.me(message.channelName, coronaStringResult);
           }
@@ -34,15 +38,21 @@ class Corona extends Command {
     }
     if (!countryName) {
       track.all().then((cInfo) => {
-        const { cases, deaths, recovered } = cInfo;
+          let { cases, deaths, recovered } = cInfo;
+	  cases = Number(cases).toLocaleString();
+	  deaths = Number(deaths).toLocaleString();
+	  recovered = Number(recovered).toLocaleString();
         this.this.bot.say(message.channelName, `The world has ${cases} confirmed cases, ${deaths} deaths, and ${recovered} recovered cases`);
       });
     } else {
       track.countries(countryName).then((cInfo) => {
         if (cInfo.message) return this.bot.me(message.channelName, `${cInfo.message} ${nam}`);
-        const {
-          country, cases, deaths, recovered,
-        } = cInfo;
+          let {
+              country, cases, deaths, recovered,
+          } = cInfo;
+	  cases = Number(cases).toLocaleString();
+	  deaths = Number(deaths).toLocaleString();
+	  recovered = Number(recovered).toLocaleString();
         const coronaStringResult = `${country} has a total of ${cases} Confirmed Cases, ${deaths} Deaths and ${recovered} Recovered`;
         this.bot.me(message.channelName, coronaStringResult);
       });
